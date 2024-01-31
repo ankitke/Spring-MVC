b484d925a97f61da47fec986471ead26283cf1a4
ankitke <ankitkumarkeshrifbg8543@gmail.com>
2024-01-31 11:39:48 +0530

    first commit

---
+++ spring_mvc3/src/main/java/com/spring/mvc/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.spring.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home")
+	public String home() {
+		
+		return "home";
+		
+	}
+	
+	@RequestMapping("/login")
+	public String loginPage() {
+		
+		return "login";
+	}
+}
